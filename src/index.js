ae0b1297a1a01a88ae744d73ee1720725b0b912a
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-11 15:15:04 +0300

    all css commits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import "./style.css";
 const header = (
-  <header>
+  <header class = 'header'>
     <h1>Welcome to 30 Days Of React</h1>
     <h2>Getting Started React</h2>
     <h3>JavaScript Library</h3>
@@ -13,12 +13,14 @@ const header = (
 
 // jsx element , Main 
 const main = (
-  <main>
+  <main class= 'main'>
     <p>Prerequisite to get started with react.js </p>
   <ul>
-    <li>HTML</li>
-    <li>css</li>
-    <li>Javascript</li>
+  <li><a href=" #">HTML</a></li>
+  <li><a href=" #">Css</a></li>
+  <li><a href=" #">JavaScript</a></li>
+  <li><a href=" #">React</a></li>
+    
   </ul>
   </main>
 )
